999e34bce21acfdb4608ea27e4549597f5e30b8e
Vedant Choudhary <vedantc6@gmail.com>
2019-11-18 23:11:21 -0500

    Create networkDelayTime.py

---
+++ Session2_2019/networkDelayTime.py
@@ -0,0 +1,58 @@
+# DFS
+class Solution(object):
+    def networkDelayTime(self, times, N, K):
+        """
+        :type times: List[List[int]]
+        :type N: int
+        :type K: int
+        :rtype: int
+        """
+        graph = collections.defaultdict(list)
+        for u, v, w in times:
+            graph[u].append((w, v))
+
+        dist = {node: float('inf') for node in range(1, N+1)}
+
+        def dfs(node, elapsed):
+            if elapsed >= dist[node]:
+                return
+            dist[node] = elapsed
+            for time, nei in sorted(graph[node]):
+                dfs(nei, elapsed+time)
+
+        dfs(K, 0)
+        ans = max(dist.values())
+        return ans if ans < float('inf') else -1
+
+# Dijkstra
+class Solution(object):
+    def networkDelayTime(self, times, N, K):
+        """
+        :type times: List[List[int]]
+        :type N: int
+        :type K: int
+        :rtype: int
+        """
+        graph = collections.defaultdict(list)
+        for u, v, w in times:
+            graph[u].append((v, w))
+
+        dist = {node: float('inf') for node in xrange(1, N+1)}
+        seen = [False] * (N+1)
+        dist[K] = 0
+        while True:
+            cand_node = -1
+            cand_dist = float('inf')
+            for i in xrange(1, N+1):
+                if not seen[i] and dist[i] < cand_dist:
+                    cand_dist = dist[i]
+                    cand_node = i
+
+            if cand_node < 0:
+                break
+            seen[cand_node] = True
+            for nei, d in graph[cand_node]:
+                dist[nei] = min(dist[nei], dist[cand_node] + d)
+
+        ans = max(dist.values())
+        return ans if ans < float('inf') else -1
